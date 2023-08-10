87f1581af017a0b80f432308614b90c1304055e0
TabriaJenae <tabriawilliams@gmail.com>
2023-08-10 10:19:51 -0400

    Authenticate Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Authenticate from './components/Authenticate';
 import SignUpForm from './components/SignUpForm';
+import React, {useState} from 'react';
 import './App.css'
 
 function App() {
- 
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <Authenticate token={token} setToken={setToken}/>
+      <SignUpForm token={token} setToken={setToken}/>
     </>
   )
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,43 +1,58 @@
 import React, {useState} from 'react';
 
-const SignUpForm = () => {
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("")
-const [error, setError] = useState(null);
+const SignUpForm = ({setToken}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
-async function handleSubmit(event) {
-    event.preventDefault();
-    console.log(username, password);
+    async function handleSubmit(event) {
+        event.preventDefault();
+    
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",
+            { 
+                method: "POST", 
+                headers: { 
+                  "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                  username: "some-username", 
+                  password: "super-secret-999" 
+                }) 
+              })
+            const result = await response.json();
+            setToken(result.token);
+            console.log(result);   
+        } catch (error) {
+          setError(error.message);
+        }
+      }
 
-try { 
-   const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`);
-   const result = await response.json(); 
-   console.log(result);
-} catch (error) {
-    setError(error.message)
-}
-}
     return ( 
-        <>
-        <h2>Sign Up!</h2> 
-        {error && <p>{error}</p>}
-         <form method='POST' onSubmit={handleSubmit}>
-       <label >
-         Username:{""} 
-         <input value = {username}
-          onChange={(e)=> 
-         setUsername(e.target.value)} />
-       </label>
-       <label >
-         Password:{""}
-          <input value={password} 
-          onChange={(e)=> 
-          setPassword(e.target.value)}/>
-       </label>
-       <button type='submit'>Submit</button>
-     </form>
-     </>
-    );
+       <>
+       <h2>Sign Up!</h2> 
+       {error && <p>{error}</p>}
+        <form method='POST' onSubmit={handleSubmit}>
+      <label >
+        Username:{""} 
+        <input 
+        value = {username}
+         onChange={(e)=> 
+        setUsername(e.target.value)} />
+      </label>
+      <label >
+        Password:{""}
+         <input 
+         type='password'
+         value={password} 
+         onChange={(e)=> 
+         setPassword(e.target.value)}/>
+      </label>
+      <button type='submit'>Submit</button>
+
+    </form>
+    </>
+ );
 }
  
 export default SignUpForm;
